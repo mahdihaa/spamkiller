38ee7dc33b568b1b22397544d19f795341c00b18
mahdihaa <mahdihasanloo79@gmail.com>
2016-05-05 12:59:31 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {164414280},--Sudo users
+    sudo_users = {110162386},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
